41ba3c72475445f41df059436b23c4aa2bd661a6
Serhat <serhatpotur1@gmail.com>
2022-06-09 21:08:37 +0300

    Template Engine ejs

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
 const express = require('express');
-const path  = require('path');
+const path = require('path');
+const ejs = require('ejs');
 const app = express();
 const port = 3000;
-
 
 //MIDDLEWARE : Req ve Res arasında ki yapılan işlerin(metot,istek vs) tümüdür
 // static dosyalar burada tanımlanır
 app.use(express.static('public'));
 
 
-app.get('/', (req, res) => {
-  // const photo = {
-  //   id: 1,
-  //   name: 'photo name',
-  //   description: 'Photo Desc.',
-  // };
-  // res.send(photo);
+// TEMPLATE ENGINE
+// mvc yapısında ki views kısmını belirtiriz. Expresse biz views olarak ejs kullanıcaz dedik
+// ejs olarak kullanacağımız için .html uzantılı dosyaları .ejs olarak değiştiriyoruz
+app.set('view engine','ejs')
 
-  res.sendFile(path.resolve(__dirname,'temp/index.html'))
+
+// ROUTES
+app.get('/', (req, res) => {
+  res.render('index')
+});
+app.get('/about', (req, res) => {
+  res.render('about')
+});
+app.get('/add', (req, res) => {
+  res.render('add')
 });
 
 app.listen(port, () => {
