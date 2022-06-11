6b04e1cd03b74708af8e865b2508d104429c01f8
Serhat <serhatpotur1@gmail.com>
2022-06-11 15:57:23 +0300

    Add Model

--- app.js
+++ app.js
@@ -1,29 +1,47 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
 const ejs = require('ejs');
+const Photo = require('./models/Photo');
 const app = express();
 const port = 3000;
+
+//connect Db
+mongoose
+  .connect('mongodb://127.0.0.1:27017/pcat-test-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Database Connected !'))
+  .catch((err) => console.log(err));
 
 //MIDDLEWARE : Req ve Res arasında ki yapılan işlerin(metot,istek vs) tümüdür
 // static dosyalar burada tanımlanır
 app.use(express.static('public'));
-
+app.use(express.urlencoded({ extended: true })); // url datayı okumamuzu sağlar
+app.use(express.json()); // urlde ki datayı json çevirir
 
 // TEMPLATE ENGINE
 // mvc yapısında ki views kısmını belirtiriz. Expresse biz views olarak ejs kullanıcaz dedik
 // ejs olarak kullanacağımız için .html uzantılı dosyaları .ejs olarak değiştiriyoruz
-app.set('view engine','ejs')
-
+app.set('view engine', 'ejs');
 
 // ROUTES
-app.get('/', (req, res) => {
-  res.render('index')
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({});
+  res.render('index',{
+    photos
+  });
 });
 app.get('/about', (req, res) => {
-  res.render('about')
+  res.render('about');
 });
 app.get('/add', (req, res) => {
-  res.render('add')
+  res.render('add');
+});
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body);
+  res.redirect('/');
 });
 
 app.listen(port, () => {
